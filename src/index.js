10ab6a0a2b58f1578f793d08497d46cd9c7834cb
Kent C. Dodds <kent@doddsfamily.us>
2016-02-16 21:07:05 -0700

    fix(export): Make it make sense

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import validators from './validators'
 import flatten from 'lodash.flatten'
 import {configValidator} from 'configuration-validator'
 
-export default webpackValidator
+module.exports = webpackValidator
+module.exports.validators = validators
 
 function webpackValidator(config, ...otherValidators) {
   const instanceValidators = flatten([...validators, ...otherValidators])
