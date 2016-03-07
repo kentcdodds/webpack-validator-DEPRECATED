82de045f788724640147cdab2b8a916c37393311
Martin Schmid <martin.schmid@live.com>
2016-03-07 20:48:10 +0100

    feat(debug): Validate debug property

---
+++ src/validators/debug/index.test.js
@@ -0,0 +1,14 @@
+import test from 'ava'
+
+import debugValidator from '.'
+const {validate} = debugValidator
+
+test('passes with a boolean value', t => {
+  const debug = false
+  t.notOk(validate(debug))
+})
+
+test('fails with a non boolean value', t => {
+  const debug = 'someValue'
+  t.ok(validate(debug))
+})
