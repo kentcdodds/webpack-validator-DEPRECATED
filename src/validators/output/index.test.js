38c1d933a5a66404114bd628bdaea8d8f14def93
Martin Schmid <martin.schmid@live.com>
2016-03-13 11:06:34 +0100

    feat(output) add validators for output.filename & output.path

---
+++ src/validators/output/index.test.js
@@ -0,0 +1,40 @@
+import test from 'ava'
+import * as outputValidators from '.'
+
+const absolutePath = process.platform === 'win32' ? 'C://foo/bar/baz' : '/foo/bar/baz'
+const relativePath = 'foo/bar'
+
+runOutputFileNameTests(outputValidators.validateOutputFileName)
+runOutputPathTests(outputValidators.validateOutputPath)
+
+function runOutputFileNameTests(validate) {
+  test('passes with a relative path', t => {
+    const output = {filename: relativePath}
+    const result = validate(output.filename)
+    t.notOk(result)
+  })
+
+  test('returns error if given an absolute path', t => {
+    const output = {filename: absolutePath}
+    const result = validate(output.filename)
+    t.ok(result)
+  })
+
+}
+
+function runOutputPathTests(validate) {
+  test('passes with an absolute path', t => {
+    const output = {filename: absolutePath}
+    const result = validate(output.filename)
+    t.notOk(result)
+  })
+
+  test('returns error if not given an absolute path', t => {
+    const output = {filename: relativePath}
+    const result = validate(output.filename)
+    t.ok(result)
+  })
+
+}
+
+
