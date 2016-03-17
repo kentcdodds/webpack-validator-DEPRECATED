0b697a6afbfd933d812f1f3b3e5660ba904113b7
Jamund Ferguson <jamuferguson@paypal.com>
2016-03-17 10:43:06 -0700

    feat(devtool): added devtool support (fixes #19)

---
+++ src/validators/devtool/index.test.js
@@ -0,0 +1,67 @@
+import test from 'ava'
+import proxyquire from 'proxyquire'
+
+const utilsStub = {fileExists: p => p.includes('exists')}
+const validateEntry = proxyquire('.', {'../../utils': utilsStub})
+
+const {validate} = validateEntry.default
+
+test('eval', t => {
+  t.notOk(validate('eval', {config: {}}))
+})
+
+test('source-map', t => {
+  t.notOk(validate('source-map', {config: {}}))
+})
+
+test('hidden-source-map', t => {
+  t.notOk(validate('hidden-source-map', {config: {}}))
+})
+
+test('inline-source-map', t => {
+  t.notOk(validate('inline-source-map', {config: {}}))
+})
+
+test('eval-source-map', t => {
+  t.notOk(validate('eval-source-map', {config: {}}))
+})
+
+test('cheap-source-map', t => {
+  t.notOk(validate('cheap-source-map', {config: {}}))
+})
+
+test('cheap-module-source-map', t => {
+  t.notOk(validate('cheap-module-source-map', {config: {}}))
+})
+
+test('cheap-eval-source-map', t => {
+  t.notOk(validate('cheap-eval-source-map', {config: {}}))
+})
+
+// pragmas
+
+test('# pragma', t => {
+  t.notOk(validate('#cheap-eval-source-map', {config: {}}))
+})
+
+test('#@ pragma', t => {
+  t.notOk(validate('#@cheap-eval-source-map', {config: {}}))
+})
+
+test('@ pragma', t => {
+  t.notOk(validate('@cheap-eval-source-map', {config: {}}))
+})
+
+// fails
+
+test('bad @@ pragma', t => {
+  t.ok(validate('@@eval', {config: {}}) === 'Unexpected devtool option @@eval')
+})
+
+test('bad devtool type pla', t => {
+  t.ok(validate('pla pla pla', {config: {}}) === 'Unexpected devtool option pla pla pla')
+})
+
+test('bad devtool type eval', t => {
+  t.ok(validate('evla-elkajslksd', {config: {}}) === 'Unexpected devtool option evla-elkajslksd')
+})
