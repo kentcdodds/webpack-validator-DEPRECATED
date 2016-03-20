7a68f08b533ddbe5604a1a46cc3452728244bcc2
Jonathan Werner <jwerner@posteo.de>
2016-03-20 20:04:43 +0100

    refactor(style): Message style

--- src/validators/resolve/validateAlias.js
+++ src/validators/resolve/validateAlias.js
@@ -3,11 +3,11 @@ import {friendlyTypeOf} from '../../utils'
 
 export default function validateAlias(alias) {
   if (!_.isPlainObject(alias)) {
-    return 'Alias should be a plain object'
+    return 'Should be a plain object'
   }
 
   if (_.some(Object.values(alias), path => friendlyTypeOf(path) !== 'string')) {
-    return `Alias object's values should be strings`
+    return `The object's values should be strings`
   }
 }
 
--- src/validators/resolve/validateAlias.test.js
+++ src/validators/resolve/validateAlias.test.js
@@ -8,13 +8,13 @@ import validateAlias from './validateAlias'
 test('fails if no plain object is passed in', t => {
   const alias = ['foo']
   const result = validateAlias(alias)
-  t.same(result, `Alias should be a plain object`)
+  t.same(result, `Should be a plain object`)
 })
 
 test(`fails if object's values aren't strings'`, t => {
   const alias = {foo: 'bar', foo$: 1}
   const result = validateAlias(alias)
-  t.same(result, `Alias object's values should be strings`)
+  t.same(result, `The object's values should be strings`)
 })
 
 
